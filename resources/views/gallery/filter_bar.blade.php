b0e79e05aedfd63918a4924820320c3eb8fc5fb9
grulovic <stefan.grulovic@gmail.com>
2021-09-28 19:16:39 +0200

    Date Autocomplete fix

--- resources/views/gallery/filter_bar.blade.php
+++ resources/views/gallery/filter_bar.blade.php
@@ -5,8 +5,8 @@
   <div class="col-lg-3 text-left">
           <div class="input btn-group mb-2">
             @php
-          
-              $url_components = parse_url($_SERVER['REQUEST_URI']); 
+
+              $url_components = parse_url($_SERVER['REQUEST_URI']);
               if( isset($url_components['query']) ){
                 $parameters = $url_components['query'];
               }
@@ -14,7 +14,7 @@
             <a href="{{ route('photos.index')}}?{{isset($parameters) ? $parameters : "" }}" class="btn btn-secondary {{ strtok($_SERVER["REQUEST_URI"], '?') == parse_url(route('photos.index'), PHP_URL_PATH) ? "active" : "" }}"><i class="fas fa-th"></i></a>
             <a href="{{ route('photos.list')}}?{{isset($parameters) ? $parameters : "" }}" class="btn btn-secondary {{ strtok($_SERVER["REQUEST_URI"], '?') == parse_url(route('photos.list'), PHP_URL_PATH) ? "active" : "" }}"><i class="fas fa-bars"></i></a>
           @if( auth()->user()->role == "admin")
-            <a href="{{ route('photos.create') }}" class="btn btn-success"><i class="fas fa-photo-video"></i> Upload gallery</a> 
+            <a href="{{ route('photos.create') }}" class="btn btn-success"><i class="fas fa-photo-video"></i> Upload gallery</a>
           @endif
           </div>
   </div>
@@ -31,11 +31,11 @@
     </div></div>
 
     <div class="col-5 col-lg-4 pl-0 pr-4 text-right"><div class="btn-group mb-2 ">
-      <button type="submit" class="btn btn-primary font-weight-bold " value="Submit">Search <i class="fas fa-search"></i></button>        
+      <button type="submit" class="btn btn-primary font-weight-bold " value="Submit">Search <i class="fas fa-search"></i></button>
       <a href="/photos" class="btn btn-danger"><i class="fas fa-times"></i></a>
     </div></div>
-    
-  </div>        
+
+  </div>
 </div>
 
   <div class="col-lg-3">
@@ -43,7 +43,7 @@
           <div class="input-group-prepend">
               <label class="input-group-text" for="from_date"><i class="far fa-calendar-alt"></i></label>
           </div>
-          <input class="form-control" type="text" id="datepicker" name="from_date" placeholder="Chose from date..." value="{{(isset(request()->from_date)) ? request()->from_date : ""}}">
+          <input class="form-control" type="text" id="datepicker" name="from_date" placeholder="Chose from date..." value="{{(isset(request()->from_date)) ? request()->from_date : ""}}"  autocomplete="off">
         </div>
   </div>
   <div class="col-lg-3">
@@ -89,8 +89,8 @@
             </select>
           </div>
 </div>
-   
+
         </form>
       </div>
-      
+
     </div>
--- resources/views/video/filter_bar.blade.php
+++ resources/views/video/filter_bar.blade.php
@@ -5,7 +5,7 @@
   <div class="col-lg-3 text-left">
           <div class="input btn-group mb-2">
             @php
-              $url_components = parse_url($_SERVER['REQUEST_URI']); 
+              $url_components = parse_url($_SERVER['REQUEST_URI']);
               if( isset($url_components['query']) ){
                 $parameters = $url_components['query'];
               }
@@ -13,7 +13,7 @@
             <a href="{{ route('videos.index')}}?{{isset($parameters) ? $parameters : "" }}" class="btn btn-secondary {{ strtok($_SERVER["REQUEST_URI"], '?') == parse_url(route('videos.index'), PHP_URL_PATH) ? "active" : "" }} "><i class="fas fa-th"></i></a>
             <a href="{{ route('videos.list')}}?{{isset($parameters) ? $parameters : "" }}" class="btn btn-secondary {{ strtok($_SERVER["REQUEST_URI"], '?') == parse_url(route('videos.list'), PHP_URL_PATH) ? "active" : "" }}"><i class="fas fa-bars"></i></a>
           @if( auth()->user()->role == "admin")
-            <a href="{{ route('videos.create') }}" class="btn btn-success"><i class="fas fa-photo-video"></i> Upload video</a> 
+            <a href="{{ route('videos.create') }}" class="btn btn-success"><i class="fas fa-photo-video"></i> Upload video</a>
           @endif
           </div>
   </div>
@@ -30,11 +30,11 @@
     </div></div>
 
     <div class="col-5 col-lg-4 pl-0 pr-4 text-right"><div class="btn-group mb-2 ">
-      <button type="submit" class="btn btn-primary font-weight-bold " value="Submit">Search <i class="fas fa-search"></i></button>        
+      <button type="submit" class="btn btn-primary font-weight-bold " value="Submit">Search <i class="fas fa-search"></i></button>
       <a href="/videos" class="btn btn-danger"><i class="fas fa-times"></i></a>
     </div></div>
-    
-  </div>        
+
+  </div>
 </div>
 
   <div class="col-lg-3">
@@ -42,7 +42,7 @@
           <div class="input-group-prepend">
               <label class="input-group-text" for="from_date"><i class="far fa-calendar-alt"></i></label>
           </div>
-          <input class="form-control" type="text" id="datepicker" name="from_date" placeholder="Chose from date..." value="{{(isset(request()->from_date)) ? request()->from_date : ""}}">
+          <input class="form-control" type="text" id="datepicker" name="from_date" placeholder="Chose from date..." value="{{(isset(request()->from_date)) ? request()->from_date : ""}}"  autocomplete="off">
         </div>
   </div>
   <div class="col-lg-3">
@@ -88,9 +88,9 @@
             </select>
           </div>
 </div>
-   
+
         </form>
       </div>
-      
+
     </div>
 
