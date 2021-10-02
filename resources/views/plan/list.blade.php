71aace750248b43a630545c47938f5685efd5826
grulovic <stefan.grulovic@gmail.com>
2021-10-02 19:50:02 +0200

    -Adding Planner Functionality V1

--- resources/views/plan/list.blade.php
+++ resources/views/plan/list.blade.php
@@ -15,7 +15,7 @@
 
       <form class="form-inline row m-0 p-0" action="{{ route('plans.index') }}" method="GET">
           @csrf
-          <input type="date" name="date" value="{{ $date }}" hidden>
+          <input type="date" name="date" value="{{ $today }}" hidden>
           <button type="submit" class="btn btn-primary" value="Submit"> <i class="fas fa-chevron-left"></i> Today </button>
       </form>
 
