a5f9822a2f6c169322ed2f6179da55bd8126228f
grulovic <stefan.grulovic@gmail.com>
2021-10-02 13:25:50 +0200

    -Adding Planner Functionality V1

--- app/Http/Controllers/PlannerController.php
+++ app/Http/Controllers/PlannerController.php
@@ -65,7 +65,7 @@ class PlannerController extends Controller
         ]);
 
         $request = $request->all();
-dd($request);
+
         unset($request['_token']);
         unset($request['_method']);
         $request['user_id'] = Auth::id();
