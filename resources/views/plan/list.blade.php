1ddd1d8a5f34d5012c556d1a912bb05d8257ea15
grulovic <stefan.grulovic@gmail.com>
2021-10-02 12:24:42 +0200

    -Adding Planner Functionality V1

--- resources/views/plan/list.blade.php
+++ resources/views/plan/list.blade.php
@@ -37,10 +37,8 @@
 
 
  <div class="col-lg-12 p-0" style="">
-            @if(isset($plans))
-            @if(count($plans) < 1)
+            @if($plans == null || sizeof($plans) <= 0 )
                <div colspan="13" class="text-center">There are no plan available yet!</div>
-			@endif
             @endif
      </div>
 
