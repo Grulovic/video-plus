ea4d68157ee46de4a37dcfabcbc6b87e03bf01e4
grulovic <stefan.grulovic@gmail.com>
2021-10-02 21:20:52 +0200

    -Adding Planner Functionality V1

--- resources/views/plan/text_card.blade.php
+++ resources/views/plan/text_card.blade.php
@@ -2,52 +2,42 @@
   <div class="card mb-4 w-100 shadow-sm">
 
    <div class="card-header">
-   
+
    <a href="{{ route('articles.show',$article->id)}}" class="text-black"><h4 class="pb-0 mb-0">
         {{ Str::limit($article->title, 60, $end='...')}}</h4></a>
-   
+
   </div>
 
-    
+
       <div class="text-muted text-right w-100 pr-2"><small>{{ date('j. F Y. H:i', strtotime($article->created_at)) }}</small></div>
     <div class="card-body pt-0" style="overflow: hidden;">
       {{--<p class="text-muted mb-0 pb-0"><small># of articles: {{ sizeof($article->articles) }}</small></p>--}}
-      <p class="text-muted" style=" white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"><small>
-        @if(sizeof($article->categories) > 0)
-          @foreach($article->categories as $category)
-          {{$category->category->title}} 
-            @if(!$loop->last) | @endif
-          @endforeach
-        @else
-          No categories
-        @endif
-      </small></p>
 
       <p class="text-muted mb-0 pb-0" style="">{!! Str::limit( strip_tags($article->description), 400, $end='...') !!}</p>
     </div>
-  
+
   <div class="text-muted text-right w-100 pr-2"><small>Views: {{$article->view_num()}}</small> </div>
    @if (!Auth::guest())
     <div class="card-footer  text-right">
         <div class="btn-group ">
-        
+
         @can('view',$article)
           <a href="{{ route('articles.show',$article->id)}}" class="btn btn-sm btn-primary"><i class="far fa-eye"></i> View</a>
         @endcan
-        
+
           <button class="btn btn-sm btn-info" onclick="copyToClipboard('{{ route('articles.show',$article->id)}}')"><i class="far fa-share-square"></i></button>
-          
+
           @can('update',$article)
           <a href="{{ route('articles.edit',$article->id)}}" class="btn btn-sm btn-warning"><i class="far fa-edit"></i></a>
 			@endcan
-        
+
         	@can('delete',$article)
            <button type="button" class="btn btn-sm  btn-danger" style="border-radius: 0 0.25rem 0.25rem 0;" data-toggle="modal" data-target="#modal_{{$article->id}}_delete_btn"  data-toggle="tooltip" data-placement="top" title="Delete Video">
                    <i class="far fa-trash-alt"></i>
                   </button>
-               
+
             @endcan
-        
+
         </div>
       </div>
   @endif
@@ -80,7 +70,7 @@
                           data-toggle="tooltip" data-placement="top" title="Delete article">
                           <i class="far fa-trash-alt"></i> Delete Article</button>
                         </form>
-                        
+
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                       </div>
                     </div>
--- resources/views/plan/video_card.blade.php
+++ resources/views/plan/video_card.blade.php
@@ -1,6 +1,6 @@
 <div class="col-sm-6 col-md-4 col-lg-3 col-xl-3 d-flex align-items-stretch p-1">
   <div class="card mb-4 w-100 shadow-sm">
-      
+
     <video id="{{$video->id}}" class="card-img-top bg-dark" poster="{{ url('uploads/videos/thumbs/thumb_'.$video->thumbnail.'_'. $video->file_name.'.png') }}"
     style="height: 190px; height:auto; min-width: 100%!important; display: block;" controls="true" playsinline muted  preload="none">
       <!-- preload="none" -->
@@ -8,27 +8,14 @@
       <source src="{{ url('uploads/videos/previews/preview_'.$video->file_name) }}" type="{{$video->mime}}">
       Your browser does not support the video tag.
     </video>
-    
-  
+
+
       <div class="text-muted text-right w-100 pr-2"><small>{{ date('j. F Y. H:i', strtotime($video->created_at)) }}</small></div>
     <div class="card-body pt-0" style="overflow: hidden;">
       <a href="{{ route('videos.show',$video->id)}}" class="text-black"><h5 class="pb-0 mb-0">{{ Str::limit($video->name, 35, $end='...')}}</h5></a>
-      <p class="text-muted" style=" white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"><small>
-        @if(sizeof($video->categories) > 0)
-          @foreach($video->categories as $category)
-          {{$category->category->title}} 
-            @if(!$loop->last) | @endif
-          @endforeach
-        @else
-          No categories
-        @endif
-      </small></p>
-    
-    	
-    
 
       <p class="text-muted mb-0 pb-0" style="">{{ Str::limit($video->description, 70, $end='...')}}</p>
-      
+
     </div>
     <div class="text-muted text-right w-100 pr-2"><small>Views: {{$video->view_num()}}</small> </div>
     <div class="card-footer  text-right">
@@ -36,9 +23,9 @@
           <a href="{{ route('videos.show',$video->id)}}" class="btn btn-sm btn-primary"   data-toggle="tooltip" data-placement="top" title="Show Video" ><i class="far fa-eye"></i> View</a>
           <a href="{{ route('videos.download',$video->id)}}" class="btn btn-sm btn-success"><i class="fas fa-download"></i></a>
           <button class="btn btn-sm btn-info" onclick="copyToClipboard('{{ route('videos.show',$video->id)}}')"><i class="far fa-share-square"></i></button>
-          
-          
-          
+
+
+
           @if( auth()->user()->role == "admin")
           <a href="{{ route('videos.edit',$video->id)}}" class="btn btn-sm btn-warning"><i class="far fa-edit"></i></a>
 
@@ -70,7 +57,7 @@
                         Are you sure you want to delete <strong>{{ $video->name }}</strong>?
                       </div>
                       <div class="modal-footer">
-                        
+
                         <form class="" action="{{ route('videos.destroy', $video->id)}}" method="post">
                           {{ csrf_field() }}
                           @method('DELETE')
@@ -78,7 +65,7 @@
                           data-toggle="tooltip" data-placement="top" title="Delete video">
                           <i class="far fa-trash-alt"></i> Delete Video</button>
                         </form>
-                        
+
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                       </div>
                     </div>
