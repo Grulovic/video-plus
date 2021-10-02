183fef0a622c5c4425fb3eca27d7e5707601c10c
grulovic <stefan.grulovic@gmail.com>
2021-10-02 15:14:51 +0200

    -Adding Planner Functionality V1

--- resources/views/plan/card.blade.php
+++ resources/views/plan/card.blade.php
@@ -28,26 +28,35 @@
             <i class="fas fa-file-alt"></i>
         @endif
 
+        @if(sizeof($plan->videoItems)>0)
         <p>Video Items:
             @foreach( $plan->videoItems as $item )
-                <a href="{{ route('videos.show',$item->item_id)}}">{{ $item->getItem->first()->name }}</a>
+                <a href="{{ route('videos.show',$item->item_id)}}">{{ $item->getItem->name }}</a>
             @endforeach
         </p>
+        @endif
+
+        @if(sizeof($plan->photoItems)>0)
         <p>Photo Items:
             @foreach( $plan->photoItems as $item )
-                <a href="{{ route('photos.show',$item->item_id)}}">{{ $item->getItem->first()->name }}</a>
+                <a href="{{ route('photos.show',$item->item_id)}}">{{ $item->getItem->name }}</a>
             @endforeach
         </p>
+         @endif
+        @if(sizeof($plan->textItems)>0)
         <p>Article Items:
             @foreach( $plan->textItems as $item )
-                <a href="{{ route('articles.show',$item->item_id)}}">{{ $item->getItem->first()->title }}</a>
+                <a href="{{ route('articles.show',$item->item_id)}}">{{ $item->getItem->title }}</a>
             @endforeach
         </p>
+        @endif
+        @if(sizeof($plan->liveItems)>0)
         <p>Live Items:
             @foreach( $plan->liveItems as $item )
-                <a href="{{ route('lives.show',$item->item_id)}}">{{ $item->getItem->first()->title }}</a>
+                <a href="{{ route('lives.show',$item->item_id)}}">{{ $item->getItem->title }}</a>
             @endforeach
         </p>
+        @endif
 
       <p class="text-muted mb-0 pb-0" style="">{{ Str::limit($plan->description, 70, $end='...')}}</p>
 
