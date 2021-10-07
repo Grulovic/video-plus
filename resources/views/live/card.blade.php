9c24c3df72adb332a9fd258b70a16ce3f1048ce9
grulovic <stefan.grulovic@gmail.com>
2021-10-07 19:22:19 +0200

    -Live Links

--- resources/views/live/card.blade.php
+++ resources/views/live/card.blade.php
@@ -12,7 +12,7 @@
       <p class="text-muted mb-0 pb-0" style="">
           @php
               foreach(preg_split("/((\r?\n)|(\r\n?))/", $live->description) as $line){
-                    if(is_link($line)){\
+                    if(is_link($line)){
                      echo '<a href="'.$line.'">'.$line.'</a><br>';
                     }else{
                      echo $line.'<br>';
