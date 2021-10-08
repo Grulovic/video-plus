2a88f511341a04fc4cfbb71fda4d16d61bcdbf9e
grulovic <stefan.grulovic@gmail.com>
2021-10-08 22:30:57 +0200

    -Fixes

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -2,7 +2,7 @@
     <div class="p-6 my-auto" style="padding-right: 50px!important;padding-left: 50px!important;">
         <div>
 {{--            <x-jet-application-logo class="block h-12 w-auto" />--}}
-            <img src="{{ asset('logo.png') }}" style="max-height: 100px; " alt="{{ config('app.name') }}" class="inline">
+            <img src="{{ asset('logo.png') }}" style="max-height: 100px; " alt="{{ config('app.name') }}" class="inline mr-2 border-right">
 
             <a href="{{route('about')}}" ><img src="{{ asset('video-plus-logo.png') }}" style="max-height: 100px; " class="inline"></a>
         </div>
