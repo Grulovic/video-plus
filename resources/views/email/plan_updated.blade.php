f1e645d2bf21ce449c7c47a50bafc6cf701152bb
grulovic <stefan.grulovic@gmail.com>
2021-10-08 22:59:06 +0200

    -Planner Mail Update

--- resources/views/email/plan_updated.blade.php
+++ resources/views/email/plan_updated.blade.php
@@ -28,7 +28,7 @@
                                            <h1 style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';color:#3d4852;font-size:18px;font-weight:bold;margin-top:0;text-align:left">New updates on the Planner</h1>
                                            <p style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';font-size:16px;line-height:1.5em;margin-top:0;text-align:left">
 											    <hr>
-											    <br>The Plan "{{$plan->title}}" has new updates!
+											    <br>The Event you have followed has new updates!
                                         		<br>
 
 
