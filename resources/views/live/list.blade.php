25a170c9a7e8e1f84b41e7cdbaaf4fecc90e3c6b
grulovic <stefan.grulovic@gmail.com>
2021-10-08 20:21:18 +0200

    -Fixes

--- resources/views/live/list.blade.php
+++ resources/views/live/list.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
-   
+
  <div class="row  m-0 p-0">
-  
+
   <div class="col-9  text-left">
-    <h2>Live List:</h2>
+    <h2>Live Streams:</h2>
   </div>
 
 
@@ -13,35 +13,35 @@
  @if (!Auth::guest())
 
   @can('create', App\Models\Live::class)
-    <a href="{{ route('lives.create') }}" class="btn btn-success float-right ml-1 "><i class="fas fa-archive"></i> Add Live</a> 
+    <a href="{{ route('lives.create') }}" class="btn btn-success float-right ml-1 "><i class="fas fa-archive"></i> Add Live</a>
 @endcan
   @endif
   </div>
 
-  
+
 </div>
 
 
     </x-slot>
 
  <div class="row m-0 p-0 pt-5 pb-5">
- 
+
    @include('alerts')
- 
+
 
             @foreach($lives as $live)
-   				
+
          			@include('live.card')
 
             @endforeach
-      
- 
- <div class="col-lg-12 p-0" style="">          
+
+
+ <div class="col-lg-12 p-0" style="">
             @if(count($lives) < 1)
                <div colspan="13" class="text-center">There are no live available yet!</div>
 			@endif
         {!! $lives->appends(request()->input())->links() !!}
-     </div> 
+     </div>
 
  </div>
 
