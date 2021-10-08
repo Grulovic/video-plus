be9540716b324e5ed9b0af8f0e965f6d3fe8f738
grulovic <stefan.grulovic@gmail.com>
2021-10-08 22:24:23 +0200

    -Fixes

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -95,15 +95,18 @@
 
 
 	<div class="row m-0 p-0 pt-4 pb-3">
-		<div class="col-lg-12 my-auto">
-			<div class="float-left">
-				<h2>Latest Videos:</h2>
-				<p>These are the latest videos uploaded.</p>
-			</div>
-			<div class="float-right mt-2">
-				<a class="btn btn-outline-primary" href="{{route('videos.index')}}">Show latest videos <i class="fas fa-angle-right"></i></a>
-			</div>
-		</div>
+        <div class="col-lg-6 my-auto">
+            <div class="text-center text-lg-left">
+                <h2>Latest Videos:</h2>
+                <p>These are the latest videos uploaded.</p>
+            </div>
+        </div>
+        <div class="col-lg-6 my-auto">
+            <div class="text-center text-lg-right mt-2 mb-4">
+                <a class="btn btn-outline-primary" href="{{route('videos.index')}}">Show latest videos <i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
+
         @if($latest_videos != null)
 			@foreach($latest_videos as $video)
 				@include('home.video_card')
@@ -124,15 +127,17 @@
 
 
 	<div class="row m-0 p-0 pt-4 pb-3">
-		<div class="col-lg-12 my-auto">
-			<div class="float-left">
-				<h2>Latest Photos:</h2>
-				<p>These are the latest photo galleries uploaded.</p>
-			</div>
-			<div class="float-right mt-2">
-				<a class="btn btn-outline-primary" href="{{route('photos.index')}}">Show latest photos <i class="fas fa-angle-right"></i></a>
-			</div>
-		</div>
+        <div class="col-lg-6 my-auto">
+            <div class="text-center text-lg-left">
+                <h2>Latest Photos:</h2>
+                <p>These are the latest photo galleries uploaded.</p>
+            </div>
+        </div>
+        <div class="col-lg-6 my-auto">
+            <div class="text-center text-lg-right mt-2 mb-4">
+                <a class="btn btn-outline-primary" href="{{route('photos.index')}}">Show latest photos <i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
         @if($latest_photos != null)
             @foreach($latest_photos as $gallery)
                 @include('home.photo_card')
