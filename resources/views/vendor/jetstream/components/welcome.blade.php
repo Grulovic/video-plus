6dddf07cff988dcfca24bcaa3e43ea581f993620
grulovic <stefan.grulovic@gmail.com>
2021-10-08 22:28:40 +0200

    -Fixes

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,8 +1,9 @@
 <div class="sm:px-20 bg-white border-b border-gray-200 grid grid-cols-1 md:grid-cols-2" style="padding-left: 0px!important; padding-right: 0px!important;">
     <div class="p-6 my-auto" style="padding-right: 50px!important;padding-left: 50px!important;">
         <div>
-            <x-jet-application-logo class="block h-12 w-auto" />
-            <a href="{{route('about')}}" ><img src="{{ asset('video-plus-logo.png') }}" style="max-height: 100px; " class="float-left"></a>
+            <x-jet-application-logo class="" />
+
+            <a href="{{route('about')}}" ><img src="{{ asset('video-plus-logo.png') }}" style="max-height: 100px; " class=""></a>
         </div>
 
         <div class="mt-8 text-2xl">
