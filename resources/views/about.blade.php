92a28e4bf6f4902bace813308b70aed88dadb731
grulovic <stefan.grulovic@gmail.com>
2021-10-09 20:51:02 +0200

    -Fixes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12" >
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" style="margin-top:65px!important;">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg ml-2 mr-2">
                 <div class="sm:px-20 bg-white border-b border-gray-200" style="padding-left: 0px!important; padding-right: 0px!important;">
                     <div class="p-6 my-auto" style="padding-right: 50px!important;padding-left: 50px!important;">
                         <div>
