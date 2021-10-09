11ce674bf254d88c7a2876b9cb0d85f999bf98ef
grulovic <stefan.grulovic@gmail.com>
2021-10-09 09:12:17 +0200

    -Mail Dispatcher - Planner Test

--- app/Http/Controllers/PlannerController.php
+++ app/Http/Controllers/PlannerController.php
@@ -320,7 +320,7 @@ class PlannerController extends Controller
         }
         elseif(  $email_push == "all" ){
 
-            $user_ids = UserPlan::where('plan_id',$plan_id)->orderBy('id','asc')->pluck('user_id')->get();
+            $user_ids = UserPlan::where('plan_id',$plan_id)->orderBy('id','asc')->pluck('user_id');
             $users = User::whereIn('id',$user_ids)->get();
 
 //            $user_plans = UserPlan::where('plan_id',$plan_id)->orderBy('id','asc')->get();
