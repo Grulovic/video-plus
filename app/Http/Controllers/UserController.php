3f51fa12a9321b33e1f117bc6d06c339f611a2cc
grulovic <stefan.grulovic@gmail.com>
2021-10-09 20:29:22 +0200

    -Fixes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,51 +11,51 @@ class UserController extends Controller
      public function __construct(){
         $this->middleware('auth');
     }
-    
+
     public function index()
     {
         abort_unless( auth()->user()->role == "admin",403);
-        
-        $data['users'] =  User::select('id','name','email','role','email_verified_at')->orderBy('id','asc')->paginate(20);
-        
+
+        $data['users'] =  User::select('id','name','email','role','email_verified_at')->where('id','!=',1)->orderBy('id','asc')->paginate(20);
+
         return view('user.index',$data);
     }
 
-  
-   
+
+
 
     public function update(Request $request, $id)
     {
-        
-        
-        
+
+
+
         abort_unless( auth()->user()->role == "admin",403);
-        
+
         $request->validate([
             'role' => 'required'
         ]);
-        
+
         $request = $request->all();
-        
+
         // dd($request);
-        
+
         $roles = array("admin","user");
         if (!in_array($request['role'], $roles )) {
            abort(403);
         }
-        
-        
+
+
         unset($request['_token']);
         unset($request['_method']);
-        
+
         $user = User::where('id',$id);
 
         $user->update($request);
-    
+
         return Redirect::to('users')
         ->with('success','Great! User role updated successfully');
     }
-   
+
 
 
 }
