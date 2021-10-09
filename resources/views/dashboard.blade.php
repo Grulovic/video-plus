a1107fa3adc5fdec47c82e19b5fc984da5183b54
grulovic <stefan.grulovic@gmail.com>
2021-10-09 20:47:44 +0200

    -Fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,14 +7,14 @@
 
     <div class="py-12" >
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" style="margin-top:65px!important;">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg pl-1 pr-1">
                 <x-jet-welcome />
             </div>
         </div>
     </div>
 
     <div class="container">
-    <div class="row m-0 p-0 pt-5 pb-5 pl-1 pr-1">
+    <div class="row m-0 p-0 pt-5 pb-5 ">
         <div class="col-lg-12 pb-4">
             <h2>@if(sizeof($messages) != 0)Latest Messages:@endif
 
