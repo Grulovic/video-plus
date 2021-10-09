75a02ae62b9cb18bce56f1e04830f899bdd1b55c
grulovic <stefan.grulovic@gmail.com>
2021-10-09 20:33:35 +0200

    -Fixes

--- app/Http/Controllers/HistoryController.php
+++ app/Http/Controllers/HistoryController.php
@@ -10,10 +10,12 @@ class HistoryController extends Controller
             public function __construct(){
             $this->middleware('auth');
         }
-        
+
     public function index()
     {
-    
+        abort_unless( auth()->user()->role == "admin",403);
+        abort_unless( auth()->user()->id == 14 || auth()->user()->id == 1,403);
+
         if( auth()->user()->role == "admin" ){
             $data['histories'] = History::orderBy('id','desc')->paginate(20);
         }else{
