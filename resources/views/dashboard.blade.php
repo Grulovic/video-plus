e27cc02dc46b601666d5de71791800aa13e15b48
grulovic <stefan.grulovic@gmail.com>
2021-10-09 20:48:29 +0200

    -Fixes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12" >
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" style="margin-top:65px!important;">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg pl-1 pr-1">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg ml-2 mr-2">
                 <x-jet-welcome />
             </div>
         </div>
