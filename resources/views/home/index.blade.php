db152c4a50a3d2d5c8d393143fa7adc894cdc989
grulovic <stefan.grulovic@gmail.com>
2021-10-12 09:31:28 +0200

    -Fixes

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -42,7 +42,7 @@
     		<div class="col-lg-{{  sizeof($lives) <= 4 && sizeof($lives) > 2 ? 12/sizeof($lives) : 4  }} mb-2 d-flex align-items-stretch" style="">
     		<div class="card shadow-lg bg-secondary">
     		    <div class="card-header text-center h4">{{$live->title}}</div>
-    			<iframe  class="card-img " style="min-height:300px;" src="{{$live->url}}?autoplay=false"></iframe>
+    			<iframe  class="card-img " style="min-height:265px;" src="{{$live->url}}?autoplay=false"></iframe>
     		</div>
     		</div>
 		@endforeach
