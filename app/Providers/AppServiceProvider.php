ea04358680f19a736a765082249647ae5c8222e8
grulovic <stefan.grulovic@gmail.com>
2021-10-25 19:01:00 +0200

    filesystem, service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     {
 
         $this->app->bind('path.public', function() {
-            return base_path().'/../video-plus/';
+            return base_path().'/../video-plus-public/';
         });
 
 
