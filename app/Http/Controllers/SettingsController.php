b4560f71e62f0a06f3a44b66215a946333ccf30c
grulovic <stefan.grulovic@gmail.com>
2021-11-14 13:15:54 +0100

    Global Settings

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -22,14 +22,14 @@ class SettingsController extends Controller
     {
         $settings = settings();
 
-//        $request->validate([
-//            'hide_videos' => 'required',
-//            'hide_articles' => 'required',
-//            'hide_photos' => 'required',
-//            'hide_live' => 'required',
-//            'logo' => 'required',
-//            'logo_footer' => 'required',
-//        ]);
+        $request->validate([
+            'hide_videos' => 'required',
+            'hide_articles' => 'required',
+            'hide_photos' => 'required',
+            'hide_lives' => 'required',
+            'logo' => 'required',
+            'logo_footer' => 'required',
+        ]);
 
         $request = $request->all();
         dd($request);
