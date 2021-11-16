651e8a2d9491f8c59b65258886636419b67838b5
grulovic <stefan.grulovic@gmail.com>
2021-11-16 20:06:45 +0100

    Global Settings

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,7 +1,8 @@
 <x-home-layout>
 
-{{--    @include('home.carousel')--}}
-
+    @if( !settings()->get('hide_videos') && !settings()->get('hide_photos') )
+        @include('home.carousel')
+    @endif
 
 	{{--<div class="row m-0 p-0 pt-4 pb-3">
 		<div class="col-lg-12 my-auto">
@@ -91,151 +92,161 @@
     </div>
 
 
-{{--    <hr>--}}
+@if( !settings()->get('hide_videos'))
+    <hr>
+
+	<div class="row m-0 p-0 pt-4 pb-3">
+        <div class="col-lg-6 my-auto">
+            <div class="text-center text-lg-left">
+                <h2>Latest Videos:</h2>
+                <p>These are the latest videos uploaded.</p>
+            </div>
+        </div>
+        <div class="col-lg-6 my-auto">
+            <div class="text-center text-lg-right mt-2 mb-4">
+                <a class="btn btn-outline-primary" href="{{route('videos.index')}}">Show more videos <i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
+
+        @if($latest_videos != null)
+			@foreach($latest_videos as $video)
+				@include('home.video_card')
+			@endforeach
+        @endif
+
+        @if($latest_videos != null)
+            @if( sizeof($latest_videos) == 0 )
+                <div class="alert alert-secondary shadow-sm w-100" role="alert" style="">
+                    There are no videos at the moment!
+                </div>
+            @endif
+        @endif
+	</div>
+@endif
 
 
 
-{{--	<div class="row m-0 p-0 pt-4 pb-3">--}}
-{{--        <div class="col-lg-6 my-auto">--}}
-{{--            <div class="text-center text-lg-left">--}}
-{{--                <h2>Latest Videos:</h2>--}}
-{{--                <p>These are the latest videos uploaded.</p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="col-lg-6 my-auto">--}}
-{{--            <div class="text-center text-lg-right mt-2 mb-4">--}}
-{{--                <a class="btn btn-outline-primary" href="{{route('videos.index')}}">Show more videos <i class="fas fa-angle-right"></i></a>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-{{--        @if($latest_videos != null)--}}
-{{--			@foreach($latest_videos as $video)--}}
-{{--				@include('home.video_card')--}}
-{{--			@endforeach--}}
-{{--        @endif--}}
-
-{{--        @if($latest_videos != null)--}}
-{{--            @if( sizeof($latest_videos) == 0 )--}}
-{{--                <div class="alert alert-secondary shadow-sm w-100" role="alert" style="">--}}
-{{--                    There are no videos at the moment!--}}
-{{--                </div>--}}
-{{--            @endif--}}
-{{--        @endif--}}
-{{--	</div>--}}
+@if( !settings()->get('hide_photos'))
+<hr>
 
 
-{{--<hr>--}}
+	<div class="row m-0 p-0 pt-4 pb-3">
+        <div class="col-lg-6 my-auto">
+            <div class="text-center text-lg-left">
+                <h2>Latest Photos:</h2>
+                <p>These are the latest photo galleries uploaded.</p>
+            </div>
+        </div>
+        <div class="col-lg-6 my-auto">
+            <div class="text-center text-lg-right mt-2 mb-4">
+                <a class="btn btn-outline-primary" href="{{route('photos.index')}}">Show more photos <i class="fas fa-angle-right"></i></a>
+            </div>
+        </div>
+        @if($latest_photos != null)
+            @foreach($latest_photos as $gallery)
+                @include('home.photo_card')
+            @endforeach
+        @endif
 
+        @if($latest_photos != null)
+            @if( sizeof($latest_photos) == 0 )
+                <div class="alert alert-secondary shadow-sm w-100" role="alert" style="">
+                    There are no photos at the moment!
+                </div>
+            @endif
+        @endif
+	</div>
+@endif
 
-{{--	<div class="row m-0 p-0 pt-4 pb-3">--}}
-{{--        <div class="col-lg-6 my-auto">--}}
-{{--            <div class="text-center text-lg-left">--}}
-{{--                <h2>Latest Photos:</h2>--}}
-{{--                <p>These are the latest photo galleries uploaded.</p>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        <div class="col-lg-6 my-auto">--}}
-{{--            <div class="text-center text-lg-right mt-2 mb-4">--}}
-{{--                <a class="btn btn-outline-primary" href="{{route('photos.index')}}">Show more photos <i class="fas fa-angle-right"></i></a>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--        @if($latest_photos != null)--}}
-{{--            @foreach($latest_photos as $gallery)--}}
-{{--                @include('home.photo_card')--}}
-{{--            @endforeach--}}
-{{--        @endif--}}
+    @if( !settings()->get('hide_articles'))
+  <hr>
 
-{{--        @if($latest_photos != null)--}}
-{{--            @if( sizeof($latest_photos) == 0 )--}}
-{{--                <div class="alert alert-secondary shadow-sm w-100" role="alert" style="">--}}
-{{--                    There are no photos at the moment!--}}
-{{--                </div>--}}
-{{--            @endif--}}
-{{--        @endif--}}
-{{--	</div>--}}
+	<div class="row m-0 p-0 pt-4 pb-3">
+		<div class="col-lg-12 my-auto">
+			<div class="float-left">
+				<h2>Latest Articles:</h2>
+				<p>These are the latest articles posted.</p>
+			</div>
+			<div class="float-right mt-2">
+				<a class="btn btn-outline-primary" href="{{route('articles.index')}}">Show latest articles <i class="fas fa-angle-right"></i></a>
+			</div>
+		</div>
+        @if($latest_articles != null)
+			@foreach($latest_articles as $article)
+				@include('article.card')
 
-{{--  <hr>--}}
+		@endforeach
+        @endif
+	</div>
+@endif
 
-{{--	<div class="row m-0 p-0 pt-4 pb-3">--}}
-{{--		<div class="col-lg-12 my-auto">--}}
-{{--			<div class="float-left">--}}
-{{--				<h2>Latest Articles:</h2>--}}
-{{--				<p>These are the latest articles posted.</p>--}}
-{{--			</div>--}}
-{{--			<div class="float-right mt-2">--}}
-{{--				<a class="btn btn-outline-primary" href="{{route('articles.index')}}">Show latest articles <i class="fas fa-angle-right"></i></a>--}}
-{{--			</div>--}}
-{{--		</div>--}}
-{{--        @if($latest_articles != null)--}}
-{{--			@foreach($latest_articles as $article)--}}
-{{--				@include('article.card')--}}
-{{--	--}}
-{{--		@endforeach--}}
-{{--        @endif--}}
-{{--	</div>--}}
+    @if( !settings()->get('hide_videos'))
+  <hr>
 
-{{--  <hr>--}}
+  	<div class="row m-0 p-0 pt-4 pb-3">
+		<div class="col-lg-12 my-auto">
+			<div class="float-left">
+				<h2>{{ $most_videos_category->title }} Videos:</h2>
+				<p>This is the most used category for videos.</p>
+			</div>
+            @if( $most_videos_category->id != 0)
+			<div class="float-right mt-2">
+				<a class="btn btn-outline-primary" href="{{route('videos.index')}}?category={{$most_videos_category->id}}">Show more {{ $most_videos_category->title }} videos <i class="fas fa-angle-right"></i></a>
+			</div>
+            @endif
+		</div>
+        @if($most_videos != null)
+            @foreach($most_videos as $video)
+				@include('home.video_card')
+			@endforeach
+        @endif
+	</div>
+@endif
 
-{{--  	<div class="row m-0 p-0 pt-4 pb-3">--}}
-{{--		<div class="col-lg-12 my-auto">--}}
-{{--			<div class="float-left">--}}
-{{--				<h2>{{ $most_videos_category->title }} Videos:</h2>--}}
-{{--				<p>This is the most used category for videos.</p>--}}
-{{--			</div>--}}
-{{--            @if( $most_videos_category->id != 0)--}}
-{{--			<div class="float-right mt-2">--}}
-{{--				<a class="btn btn-outline-primary" href="{{route('videos.index')}}?category={{$most_videos_category->id}}">Show more {{ $most_videos_category->title }} videos <i class="fas fa-angle-right"></i></a>--}}
-{{--			</div>--}}
-{{--            @endif--}}
-{{--		</div>--}}
-{{--        @if($most_videos != null)--}}
-{{--            @foreach($most_videos as $video)--}}
-{{--				@include('home.video_card')--}}
-{{--			@endforeach--}}
-{{--        @endif--}}
-{{--	</div>--}}
+    @if( !settings()->get('hide_photos'))
+<hr>
+	<div class="row m-0 p-0 pt-4 pb-3">
+		<div class="col-lg-12 my-auto">
+			<div class="float-left">
+				<h2>{{ $most_photos_category->title }} Photos:</h2>
+				<p>This is the most used category for photos.</p>
+			</div>
+            @if( $most_photos_category->id != 0)
+			<div class="float-right mt-2">
+				<a class="btn btn-outline-primary" href="{{route('photos.index')}}?category={{$most_photos_category->id}}">Show  more {{ $most_photos_category->title }} photos <i class="fas fa-angle-right"></i></a>
+			</div>
+            @endif
+		</div>
+        @if($most_photos != null)
+            @foreach($most_photos as $gallery)
+				@include('home.photo_card')
+			@endforeach
+        @endif
+	</div>
+@endif
 
+    @if( !settings()->get('hide_articles'))
+  <hr>
 
-{{--<hr>--}}
-{{--	<div class="row m-0 p-0 pt-4 pb-3">--}}
-{{--		<div class="col-lg-12 my-auto">--}}
-{{--			<div class="float-left">--}}
-{{--				<h2>{{ $most_photos_category->title }} Photos:</h2>--}}
-{{--				<p>This is the most used category for photos.</p>--}}
-{{--			</div>--}}
-{{--            @if( $most_photos_category->id != 0)--}}
-{{--			<div class="float-right mt-2">--}}
-{{--				<a class="btn btn-outline-primary" href="{{route('photos.index')}}?category={{$most_photos_category->id}}">Show  more {{ $most_photos_category->title }} photos <i class="fas fa-angle-right"></i></a>--}}
-{{--			</div>--}}
-{{--            @endif--}}
-{{--		</div>--}}
-{{--        @if($most_photos != null)--}}
-{{--            @foreach($most_photos as $gallery)--}}
-{{--				@include('home.photo_card')--}}
-{{--			@endforeach--}}
-{{--        @endif--}}
-{{--	</div>--}}
-
-{{--  <hr>--}}
-
-{{--	<div class="row m-0 p-0 pt-4 pb-3">--}}
-{{--		<div class="col-lg-12 my-auto">--}}
-{{--			<div class="float-left">--}}
-{{--				<h2>{{ $most_articles_category->title }} Articles:</h2>--}}
-{{--				<p>This is the most used category for articles.</p>--}}
-{{--			</div>--}}
-{{--            @if( $most_articles_category->id != 0)--}}
-{{--			<div class="float-right mt-2">--}}
-{{--				<a class="btn btn-outline-primary" href="{{route('articles.index')}}?category={{$most_articles_category->id}}">Show  more {{ $most_articles_category->title }} articles <i class="fas fa-angle-right"></i></a>--}}
-{{--			</div>--}}
-{{--            @endif--}}
-{{--		</div>--}}
-{{--        @if($most_articles != null)--}}
-{{--            @foreach($most_articles as $article)--}}
-{{--				@include('article.card')--}}
-{{--			@endforeach--}}
-{{--        @endif--}}
-{{--	</div>--}}
+	<div class="row m-0 p-0 pt-4 pb-3">
+		<div class="col-lg-12 my-auto">
+			<div class="float-left">
+				<h2>{{ $most_articles_category->title }} Articles:</h2>
+				<p>This is the most used category for articles.</p>
+			</div>
+            @if( $most_articles_category->id != 0)
+			<div class="float-right mt-2">
+				<a class="btn btn-outline-primary" href="{{route('articles.index')}}?category={{$most_articles_category->id}}">Show  more {{ $most_articles_category->title }} articles <i class="fas fa-angle-right"></i></a>
+			</div>
+            @endif
+		</div>
+        @if($most_articles != null)
+            @foreach($most_articles as $article)
+				@include('article.card')
+			@endforeach
+        @endif
+	</div>
+@endif
 
 {{--  <hr>--}}
 
