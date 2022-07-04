b9b6f9e71cac0179a94b737160d1bb2d1e636cb1
grulovic <stefan.grulovic@gmail.com>
2022-07-04 21:46:19 +0200

    User active permissions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -34,8 +34,6 @@ class UserController extends Controller
             'active' => ['nullable','in:0,1'],
         ]);
 
-        dd($request->get('active'));
-
         $user->role = $request->get('role') ?? $user->role;
         $user->active = $request->get('active') ?? $user->active;
         $user->save();
