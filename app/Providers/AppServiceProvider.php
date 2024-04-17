ad0266fe95b0e8bc8823714e777bd26f0ea951f1
Stefan Grulovic <stefan.grulovic@gmail.com>
2024-04-17 22:31:11 +0200

    SSL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use App\Models\Settings;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -28,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        URL::forceScheme('https');
+
         $this->app->bind('path.public', function() {
             return base_path().'/../video-plus-public/';
         });
