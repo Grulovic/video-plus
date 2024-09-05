ba5ea6c0794d15c3de1e4014898d961c75f37627
Stefan Grulovic <stefan.grulovic@gmail.com>
2024-09-05 07:32:48 +0200

    Public fixes

--- public/index.php
+++ public/index.php
@@ -1,57 +1,21 @@
 <?php
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * @package  Laravel
+ */
 
-define('LARAVEL_START', microtime(true));
+require __DIR__.'/../vendor/autoload.php';
 
-/*
-|--------------------------------------------------------------------------
-| Check If Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is maintenance / demo mode via the "down" command we
-| will require this file so that any prerendered template can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-if (file_exists(__DIR__.'/../storage/framework/maintenance.php')) {
-    require __DIR__.'/../storage/framework/maintenance.php';
-}
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
 
-// require __DIR__.'/../vendor/autoload.php';
-require __DIR__.'/../../video_gov/vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-// $app = require_once __DIR__.'/../bootstrap/app.php';
-$app = require_once __DIR__.'/../../video_gov/bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = tap($kernel->handle(
-    $request = Request::capture()
-))->send();
+$response->send();
 
 $kernel->terminate($request, $response);
